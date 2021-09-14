45ee42016bbc1a8cad74e452954f287026ab5148
Patrick Moores <patrickmoores@Patricks-MacBook-Air.local>
2021-09-14 18:30:27 -0400

    Update after 14.2.3

---
+++ Mapping_Multiple_Points/static/js/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1IjoicG1vb3JlczE4IiwiYSI6ImNrdGtlZ3ViejB2bHYzMW1zMXh2M3gxYXIifQ.qphXHNm_7wekakQBWnlaBw";
--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -3,7 +3,7 @@ console.log("working");
 
 // Create the map object with a center and zoom level - centered to
 // middle of US
-let map = L.map('mapid').setView([34.0522, -118.2437], 14);
+let map = L.map('mapid').setView([40.7, -94.5], 4);
 
 // Create the tile layer that will be the background of the map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -18,8 +18,12 @@ let cityData = cities;
 // Loop through the cities array and create one marker for each city.
 cityData.forEach(function(city) {
     console.log(city)
-    L.marker(city.location).addTo(map);
-   });
+    L.circleMarker(city.location, {
+        radius: city.population/100000
+    })
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+  .addTo(map);
+});
 
 // Add our 'graymap' tile layer to the map.
 streets.addTo(map);
